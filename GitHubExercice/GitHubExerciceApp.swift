95dfc26d609191beb8ca4215c1820069b94bd535
Florent Frossard <florentwm@gmail.com>
2023-10-10 15:03:25 +0200

    Initial Commit

---
+++ GitHubExercice/GitHubExerciceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciceApp.swift
+//  GitHubExercice
+//
+//  Created by test on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
