29def7c149506f51ab4b529c109baa4908840c61
Florent Frossard <florentwm@gmail.com>
2023-10-10 15:46:04 +0200

    Version Bleue: 1.0

--- GitHubExercice/ContentView.swift
+++ GitHubExercice/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("V1.0.0")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
     }
