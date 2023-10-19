62d4bf1e8f4422174ce4c91ad3b122cccbdf4707
Florent Frossard <florentwm@gmail.com>
2023-10-19 14:15:41 +0200

    V3.0: Purple

--- GitHubExercice/ContentView.swift
+++ GitHubExercice/ContentView.swift
@@ -10,15 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "2.circle")
+            Image(systemName: "3.circle")
                 .resizable()
                 .scaledToFit()
-            Text("Version 2.0")
+            Text("Version 3.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
         }
         .padding()
-        .foregroundStyle(.green)
+        .foregroundStyle(.purple)
     }
 }
 
