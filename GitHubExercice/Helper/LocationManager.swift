5d12525882b597afa8ff3c6b0e08c9715ba197a2
Florent Frossard <florentwm@gmail.com>
2023-10-24 15:27:54 +0200

    Création de notre location manager CoreLocation et intégration Mapkit

---
+++ GitHubExercice/Helper/LocationManager.swift
@@ -0,0 +1,9 @@
+//
+//  LocationManager.swift
+//  GitHubExercice
+//
+//  Created by test on 24/10/2023.
+//
+
+import Foundation
+import CoreLocation
---
+++ GitHubExercice/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  GitHubExercice
+//
+//  Created by test on 24/10/2023.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
