b050de0eee1a83aa031e8bdf69b72d8e6ff4cf85
Florent Frossard <florentwm@gmail.com>
2023-10-27 16:33:55 +0200

    Ajout du model pour faire des confettis !

---
+++ GitHubExercice/Views/ConfettiView.swift
@@ -0,0 +1,18 @@
+//
+//  ConfettiView.swift
+//  GitHubExercice
+//
+//  Created by test on 27/10/2023.
+//
+
+import SwiftUI
+
+struct ConfettiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConfettiView()
+}
---
+++ GitHubExercice/Views/ConfettiViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ConfettiViewModel.swift
+//  GitHubExercice
+//
+//  Created by test on 27/10/2023.
+//
+
+import Foundation
